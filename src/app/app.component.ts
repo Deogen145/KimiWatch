ee53b09cce23358cdb120c7fffb902ca36b0cf46
Deogen145 <lilith.23tz@gmail.com>
2025-11-12 08:44:35 +0700

    TopAnime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './pages/main/main.component';
 import { RouterOutlet } from "@angular/router";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, MainComponent, RouterOutlet],
+  imports: [CommonModule, HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'KimiWatch';
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const light = document.querySelector('.mouse-light') as HTMLElement;
+    if (light) {
+      light.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+    }
+  }
 }
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  animes: any[] = [];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() { // ?page=${page}
+    this.http.get<any>(`https://api.jikan.moe/v4/top/anime`)
+      .subscribe({
+        next: (Response) => {
+          this.animes = Response.data;
+          console.log(this.animes)
+        },
+        error: (err) => {
+          console.error(err)
+        }
+      })
+  }
 }
